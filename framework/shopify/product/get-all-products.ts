80242ded1ae455de66d692c3a01b56e4efb63fc0
Filip <filip.trifunovic23@gmail.com>
2022-11-17 17:14:38 +0100

    specify fetch return type

--- framework/shopify/product/get-all-products.ts
+++ framework/shopify/product/get-all-products.ts
@@ -1,29 +1,16 @@
 import getAllProductsQuery from "../utils/queries/get-all-products";
+import fetchApi from "../utils/fetch-api";
+import { ProductConnection } from "../schema";
 
-type FetchParams = {
-  query: string;
+type ReturnType = {
+  products: ProductConnection;
 };
 
-const fetchApi = async ({ query }: FetchParams) => {
-  const url = "http://localhost:4000/graphql";
-
-  const response = await fetch(url, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      query,
-    }),
+const getAllProducts = async (): Promise<any> => {
+  const { data } = await fetchApi<ReturnType>({
+    query: getAllProductsQuery,
   });
-
-  const data = await response.json();
-  return { data };
-};
-
-const getAllProducts = async (): Promise<any[]> => {
-  const products = await fetchApi({ query: getAllProductsQuery });
-  return products.data;
+  return data.products;
 };
 
 export default getAllProducts;
