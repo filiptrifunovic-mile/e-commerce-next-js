91bc57dabbe2f0174e1acd5b3fb0c0af1084ec06
Filip <filip.trifunovic23@gmail.com>
2022-11-18 12:10:31 +0100

    npm deepmerge, configuration for merging in nextJS

--- next.config.js
+++ next.config.js
@@ -1,7 +1,23 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+
+const { withFrameworkConfig } = require("./framework/common/config");
+
+// const nextConfig = {
+//   reactStrictMode: true,
+//   swcMinify: true,
+//   i18n: {
+//     locales: ["en-US"],
+//     defaultLocale: "en-US",
+//   },
+// };
+
+module.exports = withFrameworkConfig({
   reactStrictMode: true,
   swcMinify: true,
-}
+  i18n: {
+    locales: ["en-US"],
+    defaultLocale: "en-US",
+  },
+});
 
-module.exports = nextConfig
+console.log(JSON.stringify(module.exports, null, 2));
