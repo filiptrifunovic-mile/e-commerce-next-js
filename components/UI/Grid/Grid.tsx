a214e7200ec43519f9513431a1fed41840e2cc00
Filip <filip.trifunovic23@gmail.com>
2022-11-18 19:08:53 +0100

    styling images

--- components/UI/Grid/Grid.tsx
+++ components/UI/Grid/Grid.tsx
@@ -1,9 +1,19 @@
-type GridProps = {
-  children: React.ReactNode;
-};
+import style from "./Grid.module.css";
+import cn from "classnames";
+import { FC, ReactNode } from "react";
 
-const Grid = (props: GridProps) => {
-  return <div className="root-grid">{props.children}</div>;
+interface Props {
+  children: ReactNode[];
+  layout?: "A" | "B";
+}
+
+const Grid: FC<Props> = ({ children, layout = "A" }) => {
+  const rootClass = cn(style.root, {
+    [style.layoutA]: layout === "A",
+    [style.layoutB]: layout === "B",
+  });
+
+  return <div className={rootClass}>{children}</div>;
 };
 
 export default Grid;
--- components/common/Layout/Layout.tsx
+++ components/common/Layout/Layout.tsx
@@ -1,13 +1,30 @@
+// import style from "./Layout.module.css";
+
+// type LayoutProps = {
+//   children: React.ReactNode;
+// };
+
+// const Layout = (props: LayoutProps) => {
+//   return (
+//     <div className={style.root}>
+//       <main className="fit">{props.children}</main>
+//     </div>
+//   );
+// };
+
+// export default Layout;
+
 import style from "./Layout.module.css";
+import { FC, ReactNode } from "react";
 
-type LayoutProps = {
-  children: React.ReactNode;
-};
+interface LayoutProps {
+  children: ReactNode[];
+}
 
-const Layout = (props: LayoutProps) => {
+const Layout: FC<LayoutProps> = ({ children }) => {
   return (
     <div className={style.root}>
-      <main className="fit">{props.children}</main>
+      <main className="fit">{children}</main>
     </div>
   );
 };
