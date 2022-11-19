ecb48fb437a810ea97550d109efb2587522fbd5c
Filip <filip.trifunovic23@gmail.com>
2022-11-19 18:27:06 +0100

    usernav, icons

--- components/common/Navbar/Navbar.tsx
+++ components/common/Navbar/Navbar.tsx
@@ -1,11 +1,35 @@
 import { Container } from "@components/UI";
 import { FC } from "react";
+import Link from "next/link";
+import style from "./Navbar.module.css";
+import { Usernav } from "@components/common";
 
 const Navbar: FC = () => {
   return (
     <Container>
-      <div className="flex flex-row md:py-6">
-        <h1>NEXT_STORE</h1>
+      <div className={style.root}>
+        <div className="flex flex-1 items-center">
+          <Link legacyBehavior href="/">
+            <a className={style.logo}>NEXT_STORE</a>
+          </Link>
+          <nav className="ml-6 space-x-6">
+            <Link legacyBehavior href="/">
+              <a className={style.link}>All</a>
+            </Link>
+            <Link legacyBehavior href="/">
+              <a className={style.link}>Clothes</a>
+            </Link>
+            <Link legacyBehavior href="/">
+              <a className={style.link}>Accesories</a>
+            </Link>
+            <Link legacyBehavior href="/">
+              <a className={style.link}>Shoes</a>
+            </Link>
+          </nav>
+          <div className="flex flex-1 justify-end space-x-8">
+            <Usernav />
+          </div>
+        </div>
       </div>
     </Container>
   );
---
+++ components/common/Usernav/Usernav.tsx
@@ -0,0 +1,25 @@
+import { FC } from "react";
+import style from "./Usernav.module.css";
+import Link from "next/link";
+import { Bag as Cart, Heart } from "@components/icons";
+
+const Usernav: FC = () => {
+  return (
+    <nav>
+      <ul className={style.list}>
+        <li className={style.item}>
+          <Cart />
+        </li>
+        <li className={style.item}>
+          <Link legacyBehavior href="/">
+            <a>
+              <Heart />
+            </a>
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Usernav;
