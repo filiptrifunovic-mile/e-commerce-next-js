eda6861d84b0c3c18fbda3041fbd6821e7bfb551
Filip <filip.trifunovic23@gmail.com>
2022-11-21 17:49:27 +0100

    variant and option types

--- pages/products/[slug].tsx
+++ pages/products/[slug].tsx
@@ -1,3 +1,4 @@
+import { Container } from "@components/UI";
 import { getConfig } from "@framerwork/api/config";
 import { getAllProductsPaths, getProduct } from "@framerwork/product";
 import { Layout } from "components/common";
@@ -38,8 +39,61 @@ export const getStaticProps = async ({
 export default function ProductSlug({
   product,
 }: InferGetStaticPropsType<typeof getStaticProps>) {
-  console.log(JSON.stringify(product, null, 2));
-  return <div>{JSON.stringify(product, null, 2)}</div>;
+  return (
+    <Container>
+      <p>id: {product?.id}</p>
+      <p>name: {product?.name}</p>
+      <p>price value: {product?.price.value}</p>
+      <p>price currency: {product?.price.currencyCode}</p>
+      <p>description: {product?.description}</p>
+
+      <h1 className="mb-4">Options</h1>
+      <div>
+        {product?.options.map((option, index) => {
+          return (
+            <div key={index}>
+              <p>Name: {option.displayName}</p>
+              {option.values.map((value, index) => {
+                return (
+                  <div key={index}>
+                    <p>Label: {value.label}</p>
+                    <p>Hex Color: {value.hexColor}</p>
+                  </div>
+                );
+              })}
+            </div>
+          );
+        })}
+      </div>
+
+      <h1 className="mb-4">Variants</h1>
+
+      <div>
+        {product?.variants.map((variant, index) => {
+          return (
+            <div key={index}>
+              <p>Variant name: {variant.name}</p>
+              {variant.options.map((vo, index) => {
+                return (
+                  <div key={index}>
+                    <p>Name: {vo.displayName}</p>
+                    {vo.values.map((value, index) => {
+                      return (
+                        <div key={index}>
+                          <p>Label: {value.label}</p>
+                          <p>Hex Color: {value.hexColor}</p>
+                        </div>
+                      );
+                    })}
+                  </div>
+                );
+              })}
+            </div>
+          );
+        })}
+      </div>
+    </Container>
+  );
 }
 
 ProductSlug.Layout = Layout;
