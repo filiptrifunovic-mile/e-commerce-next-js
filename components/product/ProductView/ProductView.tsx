23fae5ab659c64836662c4d09b284801dc18234d
Filip <filip.trifunovic23@gmail.com>
2022-11-22 15:09:25 +0100

    choices option

--- components/product/ProductView/ProductView.tsx
+++ components/product/ProductView/ProductView.tsx
@@ -1,5 +1,5 @@
 import cn from "classnames";
-import { FC } from "react";
+import { FC, useState } from "react";
 import s from "./ProductView.module.css";
 import postcss from "postcss";
 import postcssNesting from "postcss-nesting";
@@ -14,7 +14,15 @@ interface Props {
   product: Product;
 }
 
+type AvailableChoices = "color" | "size" | string;
+
+type Choices = {
+  [P in AvailableChoices]: string;
+};
+
 const ProductView: FC<Props> = ({ product }) => {
+  const [choices, setChoices] = useState<Choices>({});
+
   return (
     <Container>
       <div className={cn(s.root, "fit")}>
@@ -50,14 +58,27 @@ const ProductView: FC<Props> = ({ product }) => {
               <div key={option.id} className="pb-4">
                 <h2 className="uppercase font-medium">{option.displayName}</h2>
                 <div className="flex flex-row py-4">
-                  {option.values.map((ov) => (
-                    <Swatch
-                      key={`${option.id}-${ov.label}`}
-                      label={ov.label}
-                      color={ov.hexColor}
-                      variant={option.displayName}
-                    />
-                  ))}
+                  {option.values.map((ov) => {
+                    const activeChoice =
+                      choices[option.displayName.toLowerCase()];
+
+                    return (
+                      <Swatch
+                        active={ov.label.toLowerCase() === activeChoice}
+                        key={`${option.id}-${ov.label}`}
+                        label={ov.label}
+                        color={ov.hexColor}
+                        variant={option.displayName}
+                        onClick={() => {
+                          setChoices({
+                            ...choices,
+                            [option.displayName.toLowerCase()]:
+                              ov.label.toLowerCase(),
+                          });
+                        }}
+                      />
+                    );
+                  })}
                 </div>
               </div>
             ))}
--- components/product/Swatch/Swatch.tsx
+++ components/product/Swatch/Swatch.tsx
@@ -1,22 +1,38 @@
 import { FC } from "react";
 import s from "./Swatch.module.css";
 import { Check } from "@components/icons";
+import cn from "classnames";
 
 interface Props {
   color?: string;
   label?: string;
   variant?: "size" | "color" | string;
+  onClick: () => void;
+  active?: boolean;
 }
 
-const Swatch: FC<Props> = ({ color, label, variant }) => {
+const Swatch: FC<Props> = ({ color, label, variant, active, ...rest }) => {
   label = label?.toLowerCase();
   variant = variant?.toLowerCase();
 
+  const rootClassName = cn(s.root, {
+    [s.active]: active,
+    [s.color]: color,
+    [s.size]: variant === "size",
+  });
+
   return (
-    <button style={color ? { backgroundColor: color } : {}} className={s.root}>
-      {/* <span>
-        <Check />
-      </span> */}
+    <button
+      style={color ? { backgroundColor: color } : {}}
+      className={rootClassName}
+      {...rest}
+    >
+      {variant === "color" && active && (
+        <span>
+          <Check />
+        </span>
+      )}
+
       {variant === "size" ? label : null}
     </button>
   );
