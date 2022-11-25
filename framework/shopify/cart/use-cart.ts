b6e3ad76ffbb478f60a4513d72d3d6d8a9af3033
Filip <filip.trifunovic23@gmail.com>
2022-11-25 22:05:14 +0100

    fetcher output description

--- framework/shopify/cart/use-cart.ts
+++ framework/shopify/cart/use-cart.ts
@@ -7,12 +7,15 @@ import {
 } from "@framework/utils";
 import { useMemo } from "react";
 import { SWRHook } from "@common/types/hooks";
+import { Checkout } from "@framework/schema";
 
 export type UseCartHookDescriptor = {
   fetcherInput: {
     checkoutId: string;
   };
-  fetcherOutput: any;
+  fetcherOutput: {
+    node: Checkout;
+  };
   data: Cart;
 };
 
@@ -23,7 +26,7 @@ export const handler: SWRHook<UseCartHookDescriptor> = {
     query: getCheckoutQuery,
   },
   async fetcher({ fetch, options, input: { checkoutId } }) {
-    let checkout;
+    let checkout: Checkout;
 
     if (checkoutId) {
       const { data } = await fetch({
@@ -35,7 +38,7 @@ export const handler: SWRHook<UseCartHookDescriptor> = {
 
       checkout = data.node;
     } else {
-      checkout = await createCheckout(fetch);
+      checkout = await createCheckout(fetch as any);
     }
 
     const cart = checkoutToCart(checkout);
